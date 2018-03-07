1d07051a6804c047dd207d628ee8c2748e1f7ac7
George <soysmile@outlook.com>
2018-03-07 18:09:20 +0200

    Initial Commit

---
+++ uran_test_app/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  uran_test_app
+//
+//  Created by George Heints on 07.03.2018.
+//  Copyright © 2018 George Heints. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
